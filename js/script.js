2fa7b2752034e80903d11c3e47fc72b46dbe94ce
Matt-1123 <matthew.russo91@gmail.com>
2018-01-27 22:34:29 -0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,12 @@
+window.alert("Welcome")
+
+
+function addText() {
+	var log=prompt("Describe your workout");
+	this.innerHTML(log);
+}
+
+/*Put img grayCircle to the right of all calendar days with #s. Write an
+open (style.display="block") and a closed (style.display="none") function
+and have the default be closed. Call the open code when there is a log entry.
+See end of Unit 02.05 in JS course for help */
